9be17d28d4f8a057b4cc49a405330bec14277350
Karen G <kgraves0701@gmail.com>
2018-04-18 23:31:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-      if @sender.balance > @amount
+      if @sender.balance > @amount && status == "pending"
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"
@@ -26,7 +26,7 @@ class Transfer
   def reverse_transfer
     if @status = "complete"
       @receiver.balance -= @amount
-      @sender.balance += @amount 
+      @sender.balance += @amount
       @status = "reversed"
     end
   end
